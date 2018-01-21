cab3d6f5a23abfa153a7e385eea0da57511dd2f5
Ting Li <oysterbaysider@gmail.com>
2018-01-22 09:58:22 +1100

    event loop

---
+++ NodejsConsoleApp/NodejsWebApp/eventloop.js
@@ -0,0 +1,15 @@
+﻿var count = 0;
+
+var t = setInterval(function () {
+    count++;
+    console.log("Interval of 10 ms, count = " + count);
+    if (count === 20) {
+        clearInterval(t);
+    }
+}, 300);
+
+setTimeout(function () {
+    console.log("Timeout at 1000 ms");
+}, 1000);
+
+console.log("Running!");
